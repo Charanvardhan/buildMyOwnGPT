76f804487fb8ee9c619f21dd571f930555a5af7d
Charanvardhan <charanvardhanreddym@gmail.com>
2025-02-27 16:41:50 -0500

    Adding positional embeddings to the extendended version to create logits.

--- bigramModel.py
+++ bigramModel.py
@@ -4,7 +4,7 @@ from torch.nn import functional as F
 
 # hyperparameters
 batch_size = 32 # how many independent sequences will we process in parallel?
-block_size = 8 # what is the maximum context length for predictions?
+blockSize = 8 # what is the maximum context length for predictions?
 max_iters = 3000
 evalInterval = 300
 learningRate = 1e-2
@@ -38,9 +38,9 @@ val_data = data[n:]
 def get_batch(split):
     # generate a small batch of data of inputs x and targets y
     data = train_data if split == 'train' else val_data
-    ix = torch.randint(len(data) - block_size, (batch_size,))
-    x = torch.stack([data[i:i+block_size] for i in ix])
-    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    ix = torch.randint(len(data) - blockSize, (batch_size,))
+    x = torch.stack([data[i:i+blockSize] for i in ix])
+    y = torch.stack([data[i+1:i+blockSize+1] for i in ix])
     x, y = x.to(device), y.to(device)
     return x, y
 
@@ -66,6 +66,7 @@ class BigramLanguageModel(nn.Module):
         # each token directly reads off the logits for the next token from a lookup table
         # create token embeddings
         self.token_embedding_table = nn.Embedding(vocabSize, nEmbd) # dont directly recreate logits we do intermediate phase 
+        self.positionEmbeddingTable = nn.Embedding(blockSize, nEmbd)
         # from token to logits we need a linear.
         self.lmHead = nn.Linear(nEmbd, vocabSize)
 
@@ -73,7 +74,9 @@ class BigramLanguageModel(nn.Module):
 
         # idx and targets are both (B,T) tensor of integers
         tokenEmbd = self.token_embedding_table(idx) # (B,T,C)
-        logits = self.lmHead(tokenEmbd) # (B,T,vocabSize)
+        positionEmbd = self.positionEmbeddingTable(torch.arange(T, device=device)) # (T,C)
+        x = tokenEmbd + positionEmbd
+        logits = self.lmHead(x) # (B,T,vocabSize)
 
         if targets is None:
             loss = None
